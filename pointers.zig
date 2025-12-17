7683bdf1ce5821160a10bacf2afb003b8178b84c
Aeshma <madhushanka2727@gmail.com>
2025-12-17 19:39:28 +0530

    check address of user

--- pointers.zig
+++ pointers.zig
@@ -1,28 +1,31 @@
 const std = @import("std");
 
-// pub fn main() void {
-//     var user = User{
-//         .id = 1,
-//         .power = 100,
-//     };
-//     user.power += 0;
-    
-//     levelUp(user);
-//     std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
-// }
-
 pub fn main() void {
     var user = User{
         .id = 1,
         .power = 100,
     };
-    std.debug.print("{*}\n{*}\n{*}\n", .{&user, &user.id, &user.power});
-    // pointers.User@16b092a60
-    // u64@16b092a60
-    // i32@16b092a68
+    user.power += 0;
+    
+    std.debug.print("main: {*}\n", .{&user});
+    
+    levelUp(user);
+    std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
 }
 
+// pub fn main() void {
+//     var user = User{
+//         .id = 1,
+//         .power = 100,
+//     };
+//     std.debug.print("{*}\n{*}\n{*}\n", .{&user, &user.id, &user.power});
+//     // pointers.User@16b092a60
+//     // u64@16b092a60
+//     // i32@16b092a68
+// }
+
 fn levelUp(user: User) void {
+    std.debug.print("levelUp: {*}\n", .{&user});
     var u = user;
     u.power += 1;
 }
