9ab4628de3b4da97065eac3a80412ff78542a69e
Aeshma <madhushanka2727@gmail.com>
2025-12-17 19:33:43 +0530

    check pointer alignment

--- pointers.zig
+++ pointers.zig
@@ -1,14 +1,25 @@
 const std = @import("std");
 
+// pub fn main() void {
+//     var user = User{
+//         .id = 1,
+//         .power = 100,
+//     };
+//     user.power += 0;
+    
+//     levelUp(user);
+//     std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
+// }
+
 pub fn main() void {
     var user = User{
         .id = 1,
         .power = 100,
     };
-    user.power += 0;
-    
-    levelUp(user);
-    std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
+    std.debug.print("{*}\n{*}\n{*}\n", .{&user, &user.id, &user.power});
+    // pointers.User@16b092a60
+    // u64@16b092a60
+    // i32@16b092a68
 }
 
 fn levelUp(user: User) void {
