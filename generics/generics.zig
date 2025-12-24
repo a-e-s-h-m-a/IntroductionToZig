7fee7a89fde8d8639e581d0453a62543477730e9
Aeshma <madhushanka2727@gmail.com>
2025-12-24 18:20:46 +0530

    return struct

--- generics/generics.zig
+++ generics/generics.zig
@@ -2,12 +2,14 @@ const std = @import("std");
 
 pub fn main() !void {
     var arr: IntArray(3) = undefined;
-    arr[0] = 1;
-    arr[1] = 10;
-    arr[2] = 1000;
+    arr.items[0] = 1;
+    arr.items[1] = 10;
+    arr.items[2] = 1000;
     std.debug.print("{any}\n", .{arr});
 }
 
 fn IntArray(comptime length: usize) type {
-    return [length]i64;
+    return struct {
+        items: [length]i64,
+    };
 }
