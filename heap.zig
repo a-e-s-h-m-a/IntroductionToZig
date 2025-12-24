9b885b98994a5911ba75747af0fc9b9528a7fb56
Aeshma <madhushanka2727@gmail.com>
2025-12-24 11:54:37 +0530

    heap

---
+++ heap.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const allocator = gpa.allocator();
+    
+    var arr = try allocator.alloc(usize, try getRandomCount());
+    defer allocator.free(arr);
+    
+    for(0..arr.len) |i| {
+        arr[i] = i;
+    }
+    std.debug.print("{any}\n", .{arr});
+}
+
+fn getRandomCount() !u8 {
+    var seed: u64 = undefined;
+    try std.posix.getrandom(std.mem.asBytes(&seed));
+    var random = std.Random.DefaultPrng.init(seed);
+    return random.random().uintAtMost(u8, 5) + 5;
+}
