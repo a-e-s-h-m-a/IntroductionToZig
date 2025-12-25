a4c4cf53d1c07a9f2342a1d63bd32ce4989c639d
Aeshma <madhushanka2727@gmail.com>
2025-12-25 18:28:17 +0530

    add minimal zig build script

--- build.zig
+++ build.zig
@@ -4,18 +4,33 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "learning",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .root_source_file = b.path("learning.zig")
-        }),
+    const mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("learning.zig"),
     });
-    //b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    
-    const run_step = b.step("run", "Start Learning...");
-    run_step.dependOn(&run_cmd.step);
+    {
+        // setup exe
+        const exe = b.addExecutable(.{
+            .name = "learning",
+            .root_module = mod,
+        });
+        //b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        const run_step = b.step("run", "Start Learning...");
+        run_step.dependOn(&run_cmd.step);
+    }
+    {
+        // setup tests
+        const tests = b.addTest(.{
+            .root_module = mod,
+        });
+        const test_cmd = b.addRunArtifact(tests);
+        test_cmd.step.dependOn(b.getInstallStep());
+        
+        const test_step = b.step("test", "Run tests...");
+        test_step.dependOn(&test_cmd.step);
+    }
 }
--- learning.zig
+++ learning.zig
@@ -44,38 +44,38 @@ pub fn main() void {
     // but couldn't figure out an honest way to avoid this detail.
     //const d_slice2 = a[1..end];
     //std.debug.print("{any}\n", .{@TypeOf(d_slice2)});
-    // 
- //    const a = [_]i32{1, 2, 3, 4, 5};
-	// var end: usize = 4;
-	// end += 1;
-	// var b = a[1..end]; // type - []const i32
-	// std.debug.print("{any}\n", .{@TypeOf(b)});
-	// b[2] = 5; // Error -> b's type []const i32
-	// 
-	// 
-	
-	// This works
-	// var a = [_]i32{1, 2, 3, 4, 5};
-	// var end: usize = 3;
-	// end += 1;
-	// const b = a[1..end];
-	// b[2] = 99;
-	
-	// var a = [_]i32{1, 2, 3, 4, 5};
-	// var end: usize = 3;
-	// end += 1;
-	// const b = a[1..end];
-	// b = b[1..]; // Error b is a const
-	// 
-	const a = [3:false]bool{false, true, false};
-	std.debug.print("{any}\n", .{std.mem.asBytes(&a)});
-	
-	//var i = 0; // comptime_int
-	//var i: usize = 0;
-	std.debug.print("{}\n", .{.{.year = 2023, .month = 8}});
+    //
+    //    const a = [_]i32{1, 2, 3, 4, 5};
+    // var end: usize = 4;
+    // end += 1;
+    // var b = a[1..end]; // type - []const i32
+    // std.debug.print("{any}\n", .{@TypeOf(b)});
+    // b[2] = 5; // Error -> b's type []const i32
+    //
+    //
+
+    // This works
+    // var a = [_]i32{1, 2, 3, 4, 5};
+    // var end: usize = 3;
+    // end += 1;
+    // const b = a[1..end];
+    // b[2] = 99;
+
+    // var a = [_]i32{1, 2, 3, 4, 5};
+    // var end: usize = 3;
+    // end += 1;
+    // const b = a[1..end];
+    // b = b[1..]; // Error b is a const
+    //
+    const a = [3:false]bool{ false, true, false };
+    std.debug.print("{any}\n", .{std.mem.asBytes(&a)});
+
+    //var i = 0; // comptime_int
+    //var i: usize = 0;
+    std.debug.print("{}\n", .{.{ .year = 2023, .month = 8 }});
 }
 
 const testing = std.testing;
 test "IntList: add" {
-    
+    try std.testing.expectEqual(false, false);
 }
